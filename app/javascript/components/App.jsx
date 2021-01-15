5e7c2bb3e576b5d40b441b89ea80f6d863b74ebd
Abhishek Goswami <abhishekgoswami@Abhisheks-MacBook-Pro.local>
2021-01-15 15:31:31 +0530

    Added tailwindcss to rails

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const App = (props) => {
   console.log("props");
   console.log(props);
-  return <div className="font-bold text-lg">App</div>;
+  return <div className="font-bold text-lg text-red-400">App</div>;
 };
 
 export default App;
