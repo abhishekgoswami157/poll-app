11323fa00a34219de0e28d2033369cedff4791fc
Abhishek Goswami <abhishekgoswami@Abhisheks-MacBook-Pro.local>
2021-01-15 15:25:37 +0530

    added react to rails

---
+++ app/javascript/components/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const App = (props) => {
+  console.log("props");
+  console.log(props);
+  return <div className="font-bold text-lg">App</div>;
+};
+
+export default App;
